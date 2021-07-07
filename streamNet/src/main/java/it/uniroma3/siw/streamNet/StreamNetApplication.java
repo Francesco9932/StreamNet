3c5f41d6914ab97ac2b236622af0421a6778c62a
Francesco9932 <72614773+Francesco9932@users.noreply.github.com>
2021-07-07 12:06:17 +0200

    creazione progetto

---
+++ streamNet/src/main/java/it/uniroma3/siw/streamNet/StreamNetApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.streamNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamNetApplication.class, args);
+	}
+
+}
---
+++ streamNet/src/test/java/it/uniroma3/siw/streamNet/StreamNetApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.streamNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
